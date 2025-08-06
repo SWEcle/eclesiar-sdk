36ef55905ed7e4b3a10c2118d72b06ef94ee5bac
jesper <jesper@0x4a45.se>
2025-08-06 17:30:08 +0200

    fix: page param

--- src/api/wars/types.ts
+++ src/api/wars/types.ts
@@ -3,5 +3,5 @@ import { WarRound, WarHit, War, GetWarsParams } from "@/types";
 export interface WarsApi {
     getWars(params: GetWarsParams): Promise<War[]>;
     getWarRounds({ war_id }: { war_id: number }): Promise<WarRound[]>;
-    getWarRoundHits({ war_round_id }: { war_round_id: number }): Promise<WarHit[]>;
+    getWarRoundHits({ war_round_id, page }: { war_round_id: number, page?: number }): Promise<WarHit[]>;
 }
