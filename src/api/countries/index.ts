a515f84325eee3393c1b8d8eee5911ab6d8e8001
jesper <jesper@0x4a45.se>
2025-12-28 07:50:24 +0100

    fix: build

--- src/api/countries/index.ts
+++ src/api/countries/index.ts
@@ -1,14 +1,16 @@
 import { getCountries } from './getCountries';
 import { getCountryRegions } from './getCountryRegions';
-
+import { getCountryTransactions } from './getTransactions';
 import type { CountriesApi } from './types';
 
 export const countriesApi: CountriesApi = {
     getCountries,
     getCountryRegions,
+    getCountryTransactions,
 }
 export type { CountriesApi };
 export {
     getCountries,
     getCountryRegions,
+    getCountryTransactions,
 }
